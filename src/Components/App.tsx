860d5a337e8149c6d7cc87b9777a075b7a8f8dcb
ag <axel.gruenert@merkkur.de>
2019-11-22 11:21:36 +0100

    restructure project tree

---
+++ src/Components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import CounterApp from "./CounterApp/CounterApp";
+
+const App: React.FC = () => {
+
+    return (
+        <CounterApp />
+    );
+};
+
+export default App;
